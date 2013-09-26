d47f2d06b6c5b46840b67fb8d7e542e46c5fb11f
Alexander Hogue <defaultnamehere@gmail.com>
2013-09-26 14:42:02 +1000

    Compile.sh works with syntax errors and checks argument arity

--- compile.sh
+++ compile.sh
@@ -1,9 +1,22 @@
 #program could be name of statement to use
 #$1 = input file
 #$2 = output filename
+
+
+if [ $# -ne 2 ]
+  then
+    echo "Wrong number of arguments!\n"
+    echo "Usage: ./compile.sh <input file path> <output file path>\n"
+    exit 1
+fi
 export CLASSPATH=".:$PWD/antlr-4.1-complete.jar:$CLASSPATH"
+export CLASSPATH=".:$PWD/build/:$CLASSPATH"
+
 alias antlr4="java -jar $PWD/antlr-4.1-complete.jar"
 alias grun='java org.antlr.v4.runtime.misc.TestRig'
 
-antlr4 Assignment2.g -o build &&  javac build/Assignment2*.java && grun Assignment2 program  < $1 > $2
+antlr4 Assignment2.g -o build;
+javac build/Assignment2*.java;
+
+java -cp build Assignment2  $1  > $2
 
